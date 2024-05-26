15fca0fe221aaa2a0f050ae7ce433752c5f23e94
Revanth Kumar <revanani1801@gmail.com>
2024-05-26 17:07:31 +0530

    token error

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -90,7 +90,6 @@ const logout = (req, res) => {
     .status(200)
     .cookie("token", "", {
       expires: new Date(Date.now()),
-      httpOnly: true,
     })
     .json({
       success: true,
--- backend/utils/jwtToken.js
+++ backend/utils/jwtToken.js
@@ -11,7 +11,6 @@ const sendToken = (user, message, res, statusCode) => {
     expires: new Date(
       Date.now() + process.env.COOKIE_EXPIRES * 24 * 60 * 60 * 1000
     ),
-    httpOnly: true,
   };
   res.status(statusCode).cookie("token", token, options).json({
     success: true,
