d430d64fa5a1a888a952819a78cf3c824b42ceb6
Revanth Kumar <revanani1801@gmail.com>
2024-05-26 16:31:11 +0530

    axios update

--- frontend/src/config/axios.js
+++ frontend/src/config/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:4000/api",
+  baseURL: "https://cool-blog-server.vercel.app/",
   withCredentials: true,
   headers: {
     "Content-Type": "multipart/form-data",
